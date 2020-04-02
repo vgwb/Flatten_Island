5aadad3d5350e3169945309822953cf3fb1432fd
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-02 12:05:27 +0100

    Added initial code to manage different platforms Added Android and Standalone Platforms Added function to return platform system language

---
+++ Assets/02_Scripts/core/platform/Platform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public abstract class Platform
+{
+	public string GetCurrentLanguage()
+	{
+		SystemLanguage systemLanguage = Application.systemLanguage;
+		if (systemLanguage == SystemLanguage.Unknown)
+		{
+			return null;
+		}
+
+		return systemLanguage.ToString();
+	}
+}
---
+++ Assets/02_Scripts/core/platform/PlatformCreator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformCreator
+{
+	public Platform CreatePlatform()
+	{
+		Platform platform;
+#if UNITY_ANDROID
+		platform = new AndroidPlatform();
+#elif UNITY_STANDALONE
+		platform = new StandalonePlatform();
+#else
+		platform = new NullPlatform();
+#endif
+		return platform;
+	}
+}
