5557c5d59ff45fdd5e8125ae7111300953c143f5
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-07 16:23:27 +0100

    Working on Suggestion Entry: - added suggestion prefab - added classes

---
+++ Assets/02_Scripts/game/suggestions/SuggestionButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuggestionButton : MonoBehaviour
+{
+	public Text buttonText;
+	public Text buttonCostText;
+	public Image buttonCostIcon;
+
+	public void SetButton(string buttonTextId, string buttonCostTextid, bool showCostIcon)
+	{
+		buttonText.text = LocalizationManager.instance.GetText(buttonTextId);
+		buttonCostText.text = LocalizationManager.instance.GetText(buttonCostTextid);
+		buttonCostIcon.gameObject.SetActive(showCostIcon);
+	}
+}
---
+++ Assets/02_Scripts/game/suggestions/SuggestionEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Messages;
+
+public class SuggestionEntry : MonoBehaviour
+{
+	public static string PREFAB = "GUI/SuggestionEntry";
+
+	public Image advisorIcon;
+	public Text title;
+	public Text description;
+	public SuggestionButton buttonOptionA;
+	public SuggestionButton buttonOptionB;
+
+	public SuggestionXmlModel suggestionXmlModel;
+
+	public void SetSuggestion(SuggestionXmlModel suggestionXmlModel)
+	{
+		this.suggestionXmlModel = suggestionXmlModel;
+
+		title.text = LocalizationManager.instance.GetText(suggestionXmlModel.name);
+	}
+
+	public void OnButtonOptionASelected()
+	{
+
+	}
+
+	public void OnButtonOptionBSelected()
+	{
+
+	}
+}
