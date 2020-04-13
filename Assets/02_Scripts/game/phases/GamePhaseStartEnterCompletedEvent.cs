7533804288485e80ec7404f6f15943a1c0d93d33
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-13 20:32:37 +0100

    Added Enter and Exit events for the Game Phase start dialog (or cinematic sequence, not decided yet).

---
+++ Assets/02_Scripts/game/phases/GamePhaseStartEnterCompletedEvent.cs
@@ -0,0 +1,12 @@
+﻿public class GamePhaseStartEnterCompletedEvent : EventObject
+{
+	public GamePhaseXmlModel gamePhaseXmlModel;
+
+	public static GamePhaseStartEnterCompletedEvent CreateInstance(GamePhaseXmlModel gamePhaseXmlModel)
+	{
+		GamePhaseStartEnterCompletedEvent eventObject = CreateInstance<GamePhaseStartEnterCompletedEvent>();
+		eventObject.name = typeof(GamePhaseStartEnterCompletedEvent).Name;
+		eventObject.gamePhaseXmlModel = gamePhaseXmlModel;
+		return eventObject;
+	}
+}
---
+++ Assets/02_Scripts/game/phases/GamePhaseStartExitCompletedEvent.cs
@@ -0,0 +1,12 @@
+﻿public class GamePhaseStartExitCompletedEvent : EventObject
+{
+	public GamePhaseXmlModel gamePhaseXmlModel;
+
+	public static GamePhaseStartExitCompletedEvent CreateInstance(GamePhaseXmlModel gamePhaseXmlModel)
+	{
+		GamePhaseStartExitCompletedEvent eventObject = CreateInstance<GamePhaseStartExitCompletedEvent>();
+		eventObject.name = typeof(GamePhaseStartEnterCompletedEvent).Name;
+		eventObject.gamePhaseXmlModel = gamePhaseXmlModel;
+		return eventObject;
+	}
+}
