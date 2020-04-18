4ceea9fe5da4503c68544c95c03e82fe1b17f7ac
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-18 21:45:06 +0100

    Added simulation iterations.

--- Assets/02_Scripts/tools/editor/GameSessionSimulator.cs
+++ Assets/02_Scripts/tools/editor/GameSessionSimulator.cs
@@ -24,68 +24,73 @@ public class GameSessionSimulator
 
 	public void Run()
 	{
-		localPlayer = new LocalPlayer();
-		localPlayer.Init();
-		localPlayer.gameSession = new GameSession();
-		List<AdvisorXmlModel> initialAdvisors = PickAdvisors();
-		localPlayer.gameSession.Initialize(initialAdvisors);
-		localPlayer.gameSession.gamePhase = new GamePhase();
-		localPlayer.gameSession.gamePhase.Start(GameSession.INITIAL_PHASE_ID, localPlayer.gameSession.day);
-
-		bool gameOver = false;
-		bool runWin = false;
-
-		while (!gameOver)
-		{
-			GameSession gameSession = localPlayer.gameSession;
-
-			if (gameSession.HasPlayerWon())
-			{
-				gameOver = true;
-				runWin = true;
-				continue;
-			}
-
-			if (gameSession.HasPlayerLose())
-			{
-				gameOver = true;
-				runWin = false;
-				continue;
-			}
-
-			if (gameSession.IsCurrentPhaseFinished())
-			{
-				int nextPhaseId = gameSession.gamePhase.GetNextPhaseId();
-				gameSession.gamePhase = new GamePhase();
-				gameSession.gamePhase.Start(nextPhaseId, gameSession.day);
-			}
-
-			gameSession.advisors = PickAdvisors();
-			int randomAdvisorIndex = RandomGenerator.GetRandom(0, gameSession.advisors.Count);
-			selectedAdvisorXmlModel = gameSession.advisors[randomAdvisorIndex];
-
-			List<SuggestionXmlModel> suggestionXmlModels = XmlModelManager.instance.FindModels<SuggestionXmlModel>((suggestionXmlModel) => suggestionXmlModel.advisorId == selectedAdvisorXmlModel.id);
-			SuggestionXmlModel selectedSuggestionXmlModel = PickNextAvailableSuggestion(suggestionXmlModels, selectedAdvisorXmlModel);
-
-			int randomOptionIndex = RandomGenerator.GetRandom(0, selectedSuggestionXmlModel.suggestionOptionsList.Count);
-			SuggestionOptionXmlModel selectedSuggestionOptionXmlModel = selectedSuggestionXmlModel.suggestionOptionsList[randomOptionIndex];
-
-			gameSession.ApplySuggestionOption(selectedSuggestionOptionXmlModel);
-			gameSession.NextDay();
-		}
-
 		StringBuilder resultString = new StringBuilder();
-		resultString.AppendLine("Simulation Over");
-		if (runWin)
-		{
-			resultString.AppendLine("Result: WON");
-		}
-		else
-		{
-			resultString.AppendLine("Result: LOSE");
-		}
 
-		resultString.AppendLine(localPlayer.gameSession.StatusString());
+		for (int i = 0; i < simulationRuns; i++)
+		{
+			localPlayer = new LocalPlayer();
+			localPlayer.Init();
+			localPlayer.gameSession = new GameSession();
+			List<AdvisorXmlModel> initialAdvisors = PickAdvisors();
+			localPlayer.gameSession.Initialize(initialAdvisors);
+			localPlayer.gameSession.gamePhase = new GamePhase();
+			localPlayer.gameSession.gamePhase.Start(GameSession.INITIAL_PHASE_ID, localPlayer.gameSession.day);
+
+			bool gameOver = false;
+			bool runWin = false;
+
+			while (!gameOver)
+			{
+				GameSession gameSession = localPlayer.gameSession;
+
+				if (gameSession.HasPlayerWon())
+				{
+					gameOver = true;
+					runWin = true;
+					continue;
+				}
+
+				if (gameSession.HasPlayerLose())
+				{
+					gameOver = true;
+					runWin = false;
+					continue;
+				}
+
+				if (gameSession.IsCurrentPhaseFinished())
+				{
+					int nextPhaseId = gameSession.gamePhase.GetNextPhaseId();
+					gameSession.gamePhase = new GamePhase();
+					gameSession.gamePhase.Start(nextPhaseId, gameSession.day);
+				}
+
+				gameSession.advisors = PickAdvisors();
+				int randomAdvisorIndex = RandomGenerator.GetRandom(0, gameSession.advisors.Count);
+				selectedAdvisorXmlModel = gameSession.advisors[randomAdvisorIndex];
+
+				List<SuggestionXmlModel> suggestionXmlModels = XmlModelManager.instance.FindModels<SuggestionXmlModel>((suggestionXmlModel) => suggestionXmlModel.advisorId == selectedAdvisorXmlModel.id);
+				SuggestionXmlModel selectedSuggestionXmlModel = PickNextAvailableSuggestion(suggestionXmlModels, selectedAdvisorXmlModel);
+
+				int randomOptionIndex = RandomGenerator.GetRandom(0, selectedSuggestionXmlModel.suggestionOptionsList.Count);
+				SuggestionOptionXmlModel selectedSuggestionOptionXmlModel = selectedSuggestionXmlModel.suggestionOptionsList[randomOptionIndex];
+
+				gameSession.ApplySuggestionOption(selectedSuggestionOptionXmlModel);
+				gameSession.NextDay();
+			}
+
+			resultString.AppendLine("Simulation Over N." + i);
+			if (runWin)
+			{
+				resultString.AppendLine("Result: WON");
+			}
+			else
+			{
+				resultString.AppendLine("Result: LOSE");
+			}
+
+			resultString.AppendLine(localPlayer.gameSession.StatusString());
+			resultString.AppendLine("");
+		}
 
 		UnityEngine.Debug.Log(resultString.ToString());
 	}
