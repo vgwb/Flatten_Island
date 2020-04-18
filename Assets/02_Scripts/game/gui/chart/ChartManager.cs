d7c5638d9d4da92a6f032cf680c4b9bab968ac5f
Pablo <parroyoc@gmail.com>
2020-04-18 09:37:48 +0100

    remove x scale factor drawing from -1 basic prediction FutureProjector as singleton drawing the patients before the prediction line

--- Assets/02_Scripts/game/gui/chart/ChartManager.cs
+++ Assets/02_Scripts/game/gui/chart/ChartManager.cs
@@ -17,12 +17,16 @@ public class ChartManager : MonoSingleton
 	public GameObject initialDot;
 	public GameObject finalDot;
 
+	// Public options
+	public float totalAnimationTime;
+	public int dashLineSpacing;
+
 	// Measures, adjusted to viewport. Act like consts but they are potentially overriden
 	private int VIEWPORT_WITDH = 2235;
 	private int WIDTH;
 	private int CURVE_MIN_WIDTH;
 	private int CURVE_MAX_WIDTH;
-	private int DAY_WIDTH_INCREMENT;
+	private float DAY_WIDTH_INCREMENT;
 
 	private const int CHART_X_MARGIN = 125;
 	private const int VIEWPORT_HEIGHT = 1242;
@@ -31,7 +35,8 @@ public class ChartManager : MonoSingleton
 	private const int MAX_Y_RANGE = HEIGHT - CAPACITY_LINE_Y;
 	private const int OVERFLOW_MIN_Y = CAPACITY_LINE_Y -15; // Just to see the circle above
 	private const int LINE_THICKNESS = 3;
-	private const float TOTAL_ANIMATION_TIME_SEC = 1.5f;
+	private const int MAX_DAYS = 100;
+	private const float EPSILON = 0.05f;
 
 	// Sprite adjustments
 	private const int PATIENTS_PANEL_OFFSET_X = -37; // to adjust the tip of the box
@@ -41,6 +46,7 @@ public class ChartManager : MonoSingleton
 	private int dayToDrawTo = -1; // The chart is being drawn up to day and day-1
     private float elapsedTime = 0.0f;
 	private bool animating = false;
+	private bool firstDrawPending = true;
 
 
 	public static ChartManager instance
@@ -54,20 +60,24 @@ public class ChartManager : MonoSingleton
 	protected override void OnMonoSingletonAwake()
 	{
 		base.OnMonoSingletonAwake();
-		dayToDrawTo = -1; // draw up to whatever day was stored, if any. Otherwise empty chart
 		AdjustToViewport();
-		UpdateFullChart(); // Could be done later, but it's safe
 	}
 
 	protected override void OnMonoSingletonUpdate()
 	{
-		elapsedTime += Time.deltaTime;
-		if (animating)
+		if (firstDrawPending && GameManager.instance.localPlayer.gameSession != null)
+		{
+			firstDrawPending = false;
+			dayToDrawTo = GameManager.instance.localPlayer.gameSession.day;
+			UpdateFullChart(); // First draw, without a day increase, from the stored state
+		}
+		else if (animating)
 		{
 			// Note: Due to the order, the chart is animated before the day increase, but values are of day+1
 			dayToDrawTo = GameManager.instance.localPlayer.gameSession.day + 1;
 
-			if (elapsedTime > TOTAL_ANIMATION_TIME_SEC)
+			elapsedTime += Time.deltaTime;
+			if (elapsedTime > totalAnimationTime)
 			{
 				UpdateFullChart();
 				animating = false;
@@ -88,12 +98,9 @@ public class ChartManager : MonoSingleton
 			VIEWPORT_WITDH = (int)(vp.x - CHART_X_MARGIN*2);
 		}
 		WIDTH = (HEIGHT * VIEWPORT_WITDH) / VIEWPORT_HEIGHT;
-		CURVE_MIN_WIDTH = (int)(WIDTH * 0.33); // tune
-		CURVE_MAX_WIDTH = (int)(WIDTH * 0.75); // tune
-		DAY_WIDTH_INCREMENT = (CURVE_MAX_WIDTH - CURVE_MIN_WIDTH) / 20; // 5%
-
-	 	Debug.Log("VIEWPORT_WITDH: " + VIEWPORT_WITDH);
-	 	Debug.Log("WIDTH: " + WIDTH);
+		CURVE_MIN_WIDTH = (int)(WIDTH * 0.11); // tune, so show a minimum line and separate indicators
+		CURVE_MAX_WIDTH = (int)(WIDTH * 0.85); // tune, to leave space for the patients indicator
+		DAY_WIDTH_INCREMENT = (CURVE_MAX_WIDTH - CURVE_MIN_WIDTH) / (float)MAX_DAYS;
 	}
 
 	public void RestartChartAnimation()
@@ -105,15 +112,7 @@ public class ChartManager : MonoSingleton
 
 	public void UpdateFullChart()
 	{
-		UpdateChart(TOTAL_ANIMATION_TIME_SEC);
-		if (CanDrawSomePeriod()) 
-		{
-			ShowPatientsIndicator();
-		}
-		else
-		{
-			HidePatientsIndicator();
-		}
+		UpdateChart(totalAnimationTime);
 	}
 
 	public void UpdateChart(float elapsedTime)
@@ -122,14 +121,16 @@ public class ChartManager : MonoSingleton
 		evolutionChartImage.sprite = CreateChartSprite(elapsedTime);
 	}
 
-	private void HidePatientsIndicator() {
+	private void HidePatientsIndicator()
+	{
 		patientsPanel.transform.localPosition = new Vector3(10000, 10000, 0f); // Push out
 	}
 
-	private void ShowPatientsIndicator() {
+	private void ShowPatientsIndicator()
+	{
 		int day = GetDayToDrawTo();
-		int patients = GameManager.instance.localPlayer.gameSession.patients[day-1];
-		bool isOverflow = patients > GameManager.instance.localPlayer.gameSession.capacity;
+		int patients = GetPatients(day-1);
+		bool isOverflow = patients > GetCurrentCapacity();
 
 		Image patientsPanelImage = patientsPanel.gameObject.GetComponent<Image>();
 		patientsPanelImage.sprite = isOverflow ? patientsOverflowImg : patientsNormalImg;
@@ -148,78 +149,100 @@ public class ChartManager : MonoSingleton
 	private Sprite CreateChartSprite(float elapsedTime)
 	{
 		Texture2D tex = GetTransparentTexture();
-		if (CanDrawSomePeriod())
+		DrawDaySegments(tex, elapsedTime);
+		PositionBeginAndEndDots(elapsedTime);
+		if (elapsedTime > GetSegmentsAnimationTime())
 		{
-			DrawDaySegments(tex, elapsedTime);
-			PositionBeginAndEndDots(elapsedTime);
+			ShowPatientsIndicator();
 		}
-		else
-		{
-			HideDot(initialDot);
-			HideDot(finalDot);
-		}
+		DrawPredictionCurve(tex, elapsedTime);
 		
 		tex.Apply();
 		return Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
 	}
 
-	private bool CanDrawSomePeriod()
-	{
-		return GameManager.instance.localPlayer.gameSession != null && 
-			GameManager.instance.localPlayer.gameSession.day > 1;
-	}
-
 	private void DrawDaySegments(Texture2D tex, float elapsedTime)
 	{
-		float animationProgress = elapsedTime / TOTAL_ANIMATION_TIME_SEC;
-		int[] patients = GameManager.instance.localPlayer.gameSession.patients;
-		int day = GetDayToDrawTo();
+		float animationProgress = Math.Min(elapsedTime / GetSegmentsAnimationTime(), 1.0f);
 		
 		// Line, as concatenation of segments. Animate segments up to some day
+		int day = GetDayToDrawTo();
 		float daysToDraw = day * animationProgress;
 		int lastFullDay = (int)Math.Truncate(daysToDraw);
 		float lastDayProportion = daysToDraw - lastFullDay;
 
-		for (int d = 1; d < lastFullDay; d++)
+		for (int d = -1; d < lastFullDay; d++)
 		{
-			DrawOneDay(tex, d, patients);
+			DrawOneDay(tex, d);
 		}
-		if (lastFullDay > 0 && lastFullDay < day && lastDayProportion > 0.05f) // TUNE, some epsilon worth drawing 
+		if (lastFullDay > 0 && lastFullDay < day && lastDayProportion > EPSILON) // TUNE, some epsilon worth drawing 
 		{
-			DrawOnePartialDay(tex, lastFullDay, patients, lastDayProportion);
+			DrawOnePartialDay(tex, lastFullDay, lastDayProportion);
 		}
 	}
 
-	private void DrawOneDay(Texture2D tex, int day, int[] patients)
+	private void DrawPredictionCurve(Texture2D tex, float elapsedTime)
+	{
+		float animationProgress = elapsedTime / totalAnimationTime;
+		
+		int day = GetDayToDrawTo() + 1;
+		float daysToDraw = MAX_DAYS * animationProgress;
+		for (int d = day; d < daysToDraw; d+=dashLineSpacing) 
+		{
+			DrawPredictionDay(tex, d);
+		}
+	}
+
+	private float GetSegmentsAnimationTime()
+	{
+		int day = GetDayToDrawTo();
+		return (totalAnimationTime * day) / (float)MAX_DAYS;
+	}
+
+	private void DrawOneDay(Texture2D tex, int day)
 	{
 		int x1 = GetXForDay(day-1);
 		int x2 = GetXForDay(day);
-		int y1 = GetYForPatients(patients[day-1]);
-		int y2 = GetYForPatients(patients[day]);
+		int y1 = GetYForPatients(GetPatients(day-1));
+		int y2 = GetYForPatients(GetPatients(day));
 		DrawSegment(tex, x1, y1, x2, y2);
 	}
 
-	private void DrawOnePartialDay(Texture2D tex, int day, int[] patients, float proportion)
+	private void DrawOnePartialDay(Texture2D tex, int day, float proportion)
 	{
 		int x1 = GetXForDay(day-1);
 		int x2 = GetXForDay(day);
-		int y1 = GetYForPatients(patients[day-1]);
-		int y2 = GetYForPatients(patients[day]);
+		int y1 = GetYForPatients(GetPatients(day-1));
+		int y2 = GetYForPatients(GetPatients(day));
 		int xp = x1 + (int)Math.Round((x2 - x1) * proportion);
 		int yp = y1 + (int)Math.Round((y2 - y1) * proportion);
 		DrawSegment(tex, x1, y1, xp, yp);
 	}
 
+	private void DrawPredictionDay(Texture2D tex, int day)
+	{
+		int x = GetXForDay(day);
+		int y = GetYForPatients(FutureProjector.instance.GetPredictedPatients(day));
+
+		Color color = Color.grey;
+		if (y == OVERFLOW_MIN_Y)
+		{
+			animating = false; // Confirm behaviour
+			color = Color.red;
+		}
+		tex.DrawFilledCircle(x, y, 2, color);
+	}
+
 	private void PositionBeginAndEndDots(float elapsedTime)
 	{		
-		float animationProgress = elapsedTime / TOTAL_ANIMATION_TIME_SEC;
-		int[] patients = GameManager.instance.localPlayer.gameSession.patients;
+		float animationProgress = elapsedTime / totalAnimationTime;
+		bool hasActualCurveFinished = elapsedTime > GetSegmentsAnimationTime();
 		int day = GetDayToDrawTo();
 		
-		PositionDot(initialDot, GetXForDay(0), GetYForPatients(patients[0]));
-		if (animationProgress > 0.99)
+		PositionDot(initialDot, GetXForDay(-1), GetYForPatients(GetPatients(-1)));
+		if (hasActualCurveFinished)
 		{
-		    PositionDot(finalDot, GetXForDay(day-1), GetYForPatients(patients[day-1]));
+		    PositionDot(finalDot, GetXForDay(day-1), GetYForPatients(GetPatients(day-1)));
 		}
 		else
 		{
@@ -241,24 +264,46 @@ public class ChartManager : MonoSingleton
 
 	private int GetXForDay(int day)
 	{
-		return day * GetDayXIncrement();
+		if (day < 0) return 0;
+		if (day == 0) return CURVE_MIN_WIDTH;
+		return CURVE_MIN_WIDTH + (int)(day * DAY_WIDTH_INCREMENT);
 	}
 
 	private int GetYForPatients(int patients)
 	{
+		if (GameManager.instance.localPlayer.gameSession == null) return HEIGHT;
+		
 		float currentCapacity = (float)GameManager.instance.localPlayer.gameSession.capacity;
 		float capacityUsage = patients / currentCapacity;
 		int y = HEIGHT - (int)(capacityUsage * MAX_Y_RANGE);
 		return (y < CAPACITY_LINE_Y) ? OVERFLOW_MIN_Y : y; // Avoid drawing too out of bounds
 	}
 
-	private int GetDayXIncrement()
+	public int GetDayToDrawTo()
 	{
-		int day = GetDayToDrawTo();
-		if (day < 1) return 0;
-		int dayTargetWidth = CURVE_MIN_WIDTH + day * DAY_WIDTH_INCREMENT;
-		int totalWidth = Math.Min(dayTargetWidth, CURVE_MAX_WIDTH);
-		return totalWidth / day; // Eg: 150x1, 85x2, 63x3, etc  
+		if (GameManager.instance.localPlayer.gameSession == null) return 1;
+		
+		return (dayToDrawTo > -1) ? dayToDrawTo : GameManager.instance.localPlayer.gameSession.day;
+	}
+
+	private int GetPatients(int day)
+	{
+		if (GameManager.instance.localPlayer.gameSession == null) return 0;
+		
+		int[] patients = GameManager.instance.localPlayer.gameSession.patients;
+		return (day < 2) ? patients[1] : patients[day];
+	}
+
+	private int GetCurrentCapacity()
+	{
+		if (GameManager.instance.localPlayer.gameSession == null) return 1000; // anything big
+		
+		return GameManager.instance.localPlayer.gameSession.capacity;
+	}
+
+	private Vector3 CoordinatesInViewport(float x, float y) {
+		float ratio = VIEWPORT_WITDH / (float)WIDTH; 
+		return new Vector3(x * ratio, y * ratio, 0);
 	}
 
 	public Texture2D GetTransparentTexture()
@@ -278,21 +323,5 @@ public class ChartManager : MonoSingleton
 	public static void DrawSegment(Texture2D tex, int x1, int y1, int x2, int y2)
 	{
 		tex.DrawThickLine(x1, y1, x2, y2, Color.black, LINE_THICKNESS);
-	}
-
-	public int GetDayToDrawTo()
-	{
-		return (dayToDrawTo > -1) ? dayToDrawTo : GameManager.instance.localPlayer.gameSession.day;
-	}
-
-	private Vector3 CoordinatesInViewport(float x, float y) {
-		float ratio = VIEWPORT_WITDH / (float)WIDTH; 
-		// float x1 = x * ratio - VIEWPORT_WITDH/2;
-		// float x1 = x * ratio - CHART_X_MARGIN;
-		float x1 = x * ratio;
-		float y1 = y * ratio;
-		
-	 	Debug.Log("orig: " + x + " , " + y + " final: " + x1 + " , " + y1);
-		return new Vector3(x1, y1, 0);
 	}
 }
