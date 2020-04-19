f56948f88466e13edb7d9fb50da21fa2dfa48887
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-19 10:28:16 +0100

    Cherry-picking Zakia commit related to Menu scene.
    I add some changes in Menu.unity and needs to approve
    
    # Conflicts:
    #	Assets/01_Scenes/Menu.unity
    #	Assets/02_Scripts/game/scenes/MenuScene.cs

---
+++ Assets/02_Scripts/SuggestionMusicUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuggestionMusicUIController : MonoBehaviour
+{
+    bool IsPuse;
+    public GameObject playBtn;
+    public GameObject PauseBtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        IsPuse = false;
+
+    }
+    
+    // for stop and run applacation
+    public void Pause()
+    {
+        IsPuse = !IsPuse;
+        if (IsPuse)
+        {
+            Time.timeScale = 0;
+            playBtn.SetActive(true);
+            PauseBtn.SetActive(false);
+
+        }
+        else if (!IsPuse)
+        {
+            Time.timeScale = 0;
+            playBtn.SetActive(false);
+            PauseBtn.SetActive(true);
+        }
+    }
+}
