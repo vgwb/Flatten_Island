588e9860c3723f8be87a1b136d95cb7d44b3f716
ZakiaPezhman <zakia.ahmadi1@gmail.com>
2020-04-20 22:54:58 +0430

    these changes needs to approve

---
+++ Assets/02_Scripts/game/MusicUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicUIController : MonoBehaviour
+{
+    bool IsPause;
+    public GameObject playButton;
+    public GameObject PauseButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        IsPause = false;
+
+    }
+    
+    // for stop and run applacation
+    public void Pause ()
+    {
+        IsPause = !IsPause;
+        if (IsPause)
+        {
+            Time.timeScale = 0;
+            playButton.SetActive(true);
+            PauseButton.SetActive(false);
+
+        }
+        else if (!IsPause)
+        {
+            Time.timeScale = 0;
+            playButton.SetActive(false);
+            PauseButton.SetActive(true);
+        }
+    }
+}
