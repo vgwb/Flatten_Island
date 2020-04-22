78840e9533940f1621136a277004433f45c24ace
ZakiaPezhman <zakia.ahmadi1@gmail.com>
2020-04-22 23:47:22 +0430

    add highScore logic

---
+++ Assets/02_Scripts/game/HighScoreText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreText : MonoBehaviour
+{
+
+    public Text highScoreText;
+
+	public void DisplayHighScoreText(){
+
+		highScoreText.text = GameManager.instance.localPlayer.highScore.day.ToString();			
+
+	}
+}
