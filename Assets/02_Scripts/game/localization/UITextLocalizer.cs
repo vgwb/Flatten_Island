4aaf07c52538dada09741db82d7109a7c4688797
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-24 22:10:00 +0100

    Added UITextLocalizer component to attach to a Text when it needs to be localized. It tooks the localization id as a parameter.

---
+++ Assets/02_Scripts/game/localization/UITextLocalizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextLocalizer : MonoBehaviour
+{
+	public string localizationId;
+
+	private Text text;
+
+	private void Awake()
+	{
+		text = GetComponent<Text>();
+	}
+
+	private void OnEnable()
+	{
+		LocalizationManager.instance.SetLocalizedText(text, localizationId);
+	}
+}
