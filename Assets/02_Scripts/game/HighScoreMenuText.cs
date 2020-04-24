5bc165c7edbb3df8e246a64020b16ec8244523f8
ZakiaPezhman <zakia.ahmadi1@gmail.com>
2020-04-25 00:15:21 +0430

    Changed the Music/Audio Volumes

---
+++ Assets/02_Scripts/game/HighScoreMenuText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreMenuText : MonoBehaviour
+{
+
+    public Text highScoreText;
+	
+	void Start()
+	{
+         DisplayHighScoreText();
+	}
+
+	public void DisplayHighScoreText(){
+
+		highScoreText.text = GameManager.instance.localPlayer.highScore.day.ToString( );			
+
+	}
+	
+}
---
+++ Assets/02_Scripts/game/MenuMusicSettings.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicSettings : MonoBehaviour
+{
+    public GameObject buttonIconOn;
+    public GameObject buttonIconOff;
+     bool isMute;
+
+    public void Start()
+    {
+                isMute = true;
+
+    }
+
+    public void MusicOnOff()
+    {
+        isMute = !isMute;
+        if(isMute){
+            
+            Time.timeScale = 0;
+            AudioManager.instance.MuteChannel(EAudioChannelType.Music, true);
+            buttonIconOn.SetActive(true);
+            buttonIconOff.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            AudioManager.instance.MuteChannel(EAudioChannelType.Music, false);
+            buttonIconOn.SetActive(false);
+            buttonIconOff.SetActive(true);
+
+        }
+            
+    }
+    public void SfxOnOff()
+    {
+        isMute = !isMute;
+        if(isMute){
+            
+            Time.timeScale = 0;
+            AudioManager.instance.MuteChannel(EAudioChannelType.Sfx, true);
+            buttonIconOn.SetActive(true);
+            buttonIconOff.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            AudioManager.instance.MuteChannel(EAudioChannelType.Sfx, false);
+            buttonIconOn.SetActive(false);
+            buttonIconOff.SetActive(true);
+
+        }
+            
+    }
+}
