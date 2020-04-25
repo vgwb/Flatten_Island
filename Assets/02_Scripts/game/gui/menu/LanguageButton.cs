a8b427ed47009c6be21dc3313d704d27e1ec9bcf
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-25 16:20:02 +0100

    Language Settings now works in the Menu.

---
+++ Assets/02_Scripts/game/gui/menu/LanguageButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Messages;
+
+public class LanguageButton : MonoBehaviour
+{
+	public string languageId;
+
+	private LocalizationXmlModel languageXmlModel;
+
+	private void Awake()
+	{
+		languageXmlModel = LocalizationManager.instance.FindLocalizationXmlModelWithLanguageId(languageId);
+	}
+
+	public void OnClick()
+	{
+		if (languageXmlModel != null)
+		{
+			LanguageSelectedEvent languageSelectedEvent = LanguageSelectedEvent.CreateInstance(languageXmlModel);
+			EventMessage languageSelectedEventMessage = new EventMessage(this, languageSelectedEvent);
+			languageSelectedEventMessage.SetMessageType(MessageType.BROADCAST);
+			EventMessageManager.instance.QueueMessage(languageSelectedEventMessage);
+		}
+	}
+}
--- Assets/02_Scripts/game/gui/menu/LanguageMenu.cs
+++ Assets/02_Scripts/game/gui/menu/LanguageMenu.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Messages;
 
 public class LanguageMenu : MonoBehaviour
 {
 	public GridLayoutGroup languagesGrid;
 
+	private void OnEnable()
+	{
+		EventMessageHandler languageSelectedMessageHandler = new EventMessageHandler(this, OnLanguageSelectedEvent);
+		EventMessageManager.instance.AddHandler(typeof(LanguageSelectedEvent).Name, languageSelectedMessageHandler);
+	}
+
+	private void OnDisable()
+	{
+		EventMessageManager.instance.RemoveHandler(typeof(LanguageSelectedEvent).Name, this);
+	}
+
+	private void OnLanguageSelectedEvent(EventMessage eventMessage)
+	{
+		ShowGrid(false);
+	}
+
 	public void OnButtonShowMenuClick()
 	{
-		languagesGrid.gameObject.SetActive(!languagesGrid.gameObject.activeInHierarchy);
+		ShowGrid(!languagesGrid.gameObject.activeInHierarchy);
 	}
+
+	private void ShowGrid(bool visible)
+	{
+		languagesGrid.gameObject.SetActive(visible);
+	}
+
 }
