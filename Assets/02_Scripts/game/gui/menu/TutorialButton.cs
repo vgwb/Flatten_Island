e4245fb85a2fcfd5558f580b1697a8ea2060d882
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-25 17:40:32 +0100

    Added logic to enable/disable tutorial in Settings. (Tutorial button disabled now because we don't have the tutorial).

---
+++ Assets/02_Scripts/game/gui/menu/TutorialButton.cs
@@ -0,0 +1,16 @@
+﻿public class TutorialButton : ToggleButton
+{
+	private void OnEnable()
+	{
+		SetState(GameManager.instance.localPlayer.playerSettings.showTutorial);
+	}
+
+	public void OnClick()
+	{
+		bool state = GetState();
+		SetState(!state);
+
+		GameManager.instance.localPlayer.playerSettings.showTutorial = GetState();
+		GameManager.instance.SavePlayer();
+	}
+}
