ffbe265d9720611c515381e93d10f0e2b967114b
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-25 16:17:08 +0100

    Now inherits from UILocalizedText to update the text automatically wnen current language changes.

--- Assets/02_Scripts/game/HighScoreText.cs
+++ Assets/02_Scripts/game/HighScoreText.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 
-public class HighScoreText : MonoBehaviour
+public class HighScoreText : UILocalizedText
 {
     public Text highScoreText;
 	public string localizationId;
 
+	private int day;
+
 	public void DisplayDayHighScore(int day)
 	{
+		this.day = day;
+		RefreshText();
+	}
+
+	protected override void RefreshText()
+	{
+		base.RefreshText();
+
 		LocalizationManager.instance.SetLocalizedText(highScoreText, localizationId);
 		highScoreText.text = LocalizationManager.instance.ReplaceIntInText(highScoreText.text, day);
 	}
