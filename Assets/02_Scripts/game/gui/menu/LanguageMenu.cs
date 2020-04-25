b0f250bd42dcc37ed5ce5ede21ea878f68c24f88
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-25 15:10:14 +0100

    Setting up Language Menu.

---
+++ Assets/02_Scripts/game/gui/menu/LanguageMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageMenu : MonoBehaviour
+{
+	public GridLayoutGroup languagesGrid;
+
+	public void OnButtonShowMenuClick()
+	{
+		languagesGrid.gameObject.SetActive(!languagesGrid.gameObject.activeInHierarchy);
+	}
+}
