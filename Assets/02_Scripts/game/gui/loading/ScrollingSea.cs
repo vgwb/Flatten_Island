944ea08b83db7de430ead382a0b02dd24dfa710d
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-26 16:25:05 +0100

    Added Scroling Sea component.

---
+++ Assets/02_Scripts/game/gui/loading/ScrollingSea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingSea : MonoBehaviour
+{
+	public RawImage seaImage;
+	public float speed;
+
+	private Rect original_uv_rect;
+
+	private void Awake()
+	{
+		original_uv_rect = seaImage.uvRect;
+	}
+
+	private void OnEnable()
+	{
+		seaImage.uvRect = original_uv_rect;		
+	}
+
+	private void Update()
+	{
+		Rect rect = seaImage.uvRect;
+		rect.x += speed;
+		seaImage.uvRect = rect;
+	}
+}
