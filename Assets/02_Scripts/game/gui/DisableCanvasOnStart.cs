3c370dbe0e4d9e8279989d3bbc8ac1d5c536438d
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-26 19:42:10 +0100

    Added script to disable a list of canvas when the game object starts.
    Canvas can be re-enabled in two ways:
    - After a certain amount of time (set the flag to true and put a value in seconds in the timer)
    - Using an event in Animations. The name of the function to put in the event is "AnimationEventEnableImages". Set the Int value to 0 if you want to disable them or 1 if you want to enable them.

---
+++ Assets/02_Scripts/game/gui/DisableCanvasOnStart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DisableCanvasOnStart : MonoBehaviour
+{
+	public Canvas[] canvasList;
+	public bool useTimer;
+	public float timer;
+
+	private void Start()
+	{
+		EnableImages(false);
+
+		if (useTimer)
+		{
+			StartCoroutine(EnableImages());
+		}
+	}
+
+	private IEnumerator EnableImages()
+	{
+		yield return new WaitForSeconds(timer);
+
+		EnableImages(true);
+	}
+
+	private void EnableImages(bool enabled)
+	{
+		foreach (Canvas canvas in canvasList)
+		{
+			canvas.enabled = enabled;
+		}
+	}
+
+	public void AnimationEventEnableImages(int enabled)
+	{
+		EnableImages(enabled != 0);
+	}
+}
