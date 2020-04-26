ba112521dd38b86e8d7348880f8a2345064a7382
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-26 11:01:53 +0100

    Added PlayAudioClip Ingredient to play a one shot audio clip inside a recipe.

---
+++ Assets/02_Scripts/core/ai/cookbook/PlayAudioClipIngredient.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayAudioClipIngredient : Ingredient
+{
+	public AudioClip audioClip;
+
+	public override CookbookStatus Use(float deltaTime)
+	{
+		base.Use(deltaTime);
+		if (status == CookbookStatus.Running)
+		{
+			if (audioClip != null)
+			{
+				AudioManager.instance.PlayOneShot(audioClip, EAudioChannelType.Sfx);
+			}
+
+			status = CookbookStatus.Success;
+		}
+
+		return status;
+	}
+}
