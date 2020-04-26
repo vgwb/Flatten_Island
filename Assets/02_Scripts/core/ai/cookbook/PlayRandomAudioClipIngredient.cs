941ea944da9ed5f3ced623c880d133001b11e6f8
Federico Cicchi <federico.cicchi@gmail.com>
2020-04-26 11:33:13 +0100

    Added ingredient to play a randio audio clip in a recipe.

---
+++ Assets/02_Scripts/core/ai/cookbook/PlayRandomAudioClipIngredient.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayRandomAudioClipIngredient : Ingredient
+{
+	public AudioClip[] audioClips;
+
+	private ShuffleBag<AudioClip> shuffleBag;
+
+	private void Awake()
+	{
+		shuffleBag = new ShuffleBag<AudioClip>(3);
+		foreach (AudioClip audioClip in audioClips)
+		{
+			shuffleBag.Add(audioClip, 1);
+		}
+
+		shuffleBag.Shuffle();
+	}
+
+	public override CookbookStatus Use(float deltaTime)
+	{
+		base.Use(deltaTime);
+		if (status == CookbookStatus.Running)
+		{
+			AudioClip audioClip = shuffleBag.Next();
+			AudioManager.instance.PlayOneShot(audioClip, EAudioChannelType.Sfx);
+			status = CookbookStatus.Success;
+		}
+
+		return status;
+	}
+}
