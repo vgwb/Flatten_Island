2cb6bbf307c9406a7ba7727d4079b1f012903c5c
Federico Cicchi <federico.cicchi@gmail.com>
2020-05-09 12:02:04 +0100

    Changed the introduction dialog (blue one) with another one with the avatar's color + the avatar bubble on top.

--- Assets/02_Scripts/game/tutorial/TutorialDialog.cs
+++ Assets/02_Scripts/game/tutorial/TutorialDialog.cs
@@ -5,16 +5,40 @@ using Messages;
 
 public class TutorialDialog : MonoBehaviour
 {
-	public static string PREFAB = "GUI/TutorialDialog";
+	public static string ADVISO_PRESENTATION_PREFAB = "GUI/TutorialDialog";
+	public static string TUTORIAL_TIP_PREFAB = "GUI/TutorialTipDialog";
 
 	public Text presentationText;
 	public TutorialDialogChef tutorialDialogChef;
 	public AdvisorXmlModel advisorXmlModel;
+	public Image advisorIcon;
+	public Image background;
+	public Image advisorBubble;
+
+	private string shownTutorialDialogPrefab;
 
 	public void SetAdvisorDialog(AdvisorXmlModel advisorXmlModel)
 	{
 		this.advisorXmlModel = advisorXmlModel;
 		LocalizationManager.instance.SetLocalizedText(presentationText, advisorXmlModel.presentation);
+
+		if (!string.IsNullOrEmpty(advisorXmlModel.iconSprite))
+		{
+			Sprite advisorSprite = Resources.Load<Sprite>(advisorXmlModel.iconSprite);
+			advisorIcon.overrideSprite = advisorSprite;
+		}
+
+		if (!string.IsNullOrEmpty(advisorXmlModel.bubbleSprite))
+		{
+			Sprite bubbleSprite = Resources.Load<Sprite>(advisorXmlModel.bubbleSprite);
+			advisorBubble.overrideSprite = bubbleSprite;
+		}
+
+		if (!string.IsNullOrEmpty(advisorXmlModel.suggestionBackgroundSprite))
+		{
+			Sprite suggestionBackgroundSprite = Resources.Load<Sprite>(advisorXmlModel.suggestionBackgroundSprite);
+			background.overrideSprite = suggestionBackgroundSprite;
+		}
 	}
 
 	public void SetTutorialTipDialog(string localizationId)
@@ -48,15 +72,16 @@ public class TutorialDialog : MonoBehaviour
 
 	public static TutorialDialog ShowAdvisorPresentation(AdvisorXmlModel advisorXmlModel, Transform parent)
 	{
-		TutorialDialog advisorPresentationDialog = CreateDialog(parent);
+		TutorialDialog advisorPresentationDialog = CreateDialog(parent, ADVISO_PRESENTATION_PREFAB);
 		advisorPresentationDialog.SetAdvisorDialog(advisorXmlModel);
 		advisorPresentationDialog.PlayEnterRecipe();
+		advisorPresentationDialog.shownTutorialDialogPrefab = ADVISO_PRESENTATION_PREFAB;
 		return advisorPresentationDialog;
 	}
 
-	private static TutorialDialog CreateDialog(Transform parent)
+	private static TutorialDialog CreateDialog(Transform parent, string prefab)
 	{
-		GameObject presentationDialogObject = GameObjectFactory.instance.InstantiateGameObject(PREFAB, parent, false);
+		GameObject presentationDialogObject = GameObjectFactory.instance.InstantiateGameObject(prefab, parent, false);
 		presentationDialogObject.transform.SetParent(parent, true);
 		TutorialDialog advisorPresentationDialog = presentationDialogObject.GetComponent<TutorialDialog>();
 		presentationDialogObject.gameObject.SetActive(true);
@@ -66,15 +91,16 @@ public class TutorialDialog : MonoBehaviour
 
 	public static TutorialDialog ShowTutorialTip(string localizationId, Transform parent)
 	{
-		TutorialDialog advisorPresentationDialog = CreateDialog(parent);
-		advisorPresentationDialog.SetTutorialTipDialog(localizationId);
-		advisorPresentationDialog.PlayEnterRecipe();
-		return advisorPresentationDialog;
+		TutorialDialog tutorialTipDialog = CreateDialog(parent, TUTORIAL_TIP_PREFAB);
+		tutorialTipDialog.SetTutorialTipDialog(localizationId);
+		tutorialTipDialog.PlayEnterRecipe();
+		tutorialTipDialog.shownTutorialDialogPrefab = TUTORIAL_TIP_PREFAB;
+		return tutorialTipDialog;
 	}
 
 	public void Release()
 	{
 		gameObject.SetActive(false);
-		GameObjectFactory.instance.ReleaseGameObject(gameObject, PREFAB);
+		GameObjectFactory.instance.ReleaseGameObject(gameObject, shownTutorialDialogPrefab);
 	}
 }
